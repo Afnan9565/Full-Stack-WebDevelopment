8aefeb2152ba94732ce81e7121c130c9771d1e44
Afnan Ahmad <afnanahmad9565@gmail.com>
2023-07-11 16:32:57 +0530

    Question 2

--- Assignment/JS 02.js
+++ Assignment/JS 02.js
@@ -6,3 +6,9 @@ if(num%10 === 0){
 else{
     console.log("Bad")
 }
+
+// Question 2
+let userName = prompt("Enter your Name");
+let userAge = prompt("Enter your age");
+let output = `${userName} is ${userAge} years old`;
+alert(output);
