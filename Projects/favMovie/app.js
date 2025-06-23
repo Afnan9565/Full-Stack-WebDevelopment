ae04335c57355cca2973b09f48bad562164f9bf2
Afnan Ahmad <afnanahmad9565@gmail.com>
2025-06-23 09:36:10 +0530

    2 Mini Projects

---
+++ Projects/favMovie/app.js
@@ -0,0 +1,18 @@
+const favMovie = "Avatar";
+function startGame(){
+        let guess = prompt("Enter your movie name");
+        while(true){
+            
+            if(guess == "quit"){
+                console.log('You quit the game');
+                break;
+            }
+            else if(guess == favMovie){
+                alert("You Won!!!!!!!!");
+                break;
+            }
+            else{
+                guess = prompt("Wrong guess! Please try again");
+            }
+        }
+}
