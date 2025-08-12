b34696cc515615da8b95ec46b4bebc0a101634d1
Afnan Ahmad <afnanahmad9565@gmail.com>
2025-08-12 07:46:18 +0530

    Initialized Express Directory

--- Playground/app.js
+++ Playground/app.js
@@ -1,6 +1,28 @@
-inp = document.querySelector("input");
+let url = "https://catfact.ninja/fact";
+// fetch(url)
+//   .then((res) => {
+//     // console.log(res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data.fact);
+//     return fetch(url);
+//   })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data2) => {
+//     console.log(data2.fact);
+//   })
+//   .catch((err) => {
+//     console.log("Our Error ", err);
+//   });
 
-inp.addEventListener("keydown", function (e) {
-  //   console.log("key was pressed");
-  console.log(e);
-});
+async function getFacts() {
+  try {
+    let res = await axios.get(url);
+    console.log(res.data.fact);
+  } catch (e) {
+    console.log("ERROR", e);
+  }
+}
